74657072563a77205662c85011a93432c0eb8b23
Perimetro20 <a01205778@itesm.mx>
2017-01-19 19:58:00 -0600

    Fixed the problems with flake8 compliance

--- user_profiles/models.py
+++ user_profiles/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 
 class Student(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	home_directory = models.OneToOneField('spaced_repetition.Folder')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    home_directory = models.OneToOneField('spaced_repetition.Folder')
