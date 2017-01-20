b4f0554fc3d7fd44e5d1ab22f7801029ce1a521d
Perimetro20 <a01205778@itesm.mx>
2017-01-19 18:33:53 -0600

    Relationship between student and his cards for spaced repetition completed

--- spaced_repetition/models.py
+++ spaced_repetition/models.py
@@ -1,23 +1,26 @@
 import datetime
 from django.db import models
-from django.contrib.auth.models import User
+
+from user_profiles.models import Student
 
 
 class Folder(models.Model):
-    parent = models.ForeignKey('self')
+    owner = models.ForeignKey(Student)
+    parent_directory = models.ForeignKey('self')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Deck(models.Model):
-    user = models.ForeignKey(User)
-    parent = models.ForeignKey(Folder)
+    owner = models.ForeignKey(Student)
+    parent_directory = models.ForeignKey(Folder)
     name = models.DateTimeField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Card(models.Model):
+    owner = models.ForeignKey(Student)
     deck = models.ForeignKey(Deck)
     question = models.CharField(max_length=300)
     answer = models.CharField(max_length=750)
